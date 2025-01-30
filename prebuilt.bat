caa82cf10517defdfc7a3654aa6291c0a41442a9
olegmicode <olegmijovic.io@gmail.com>
2025-01-30 01:27:47 +0000

    fix the issue

--- prebuilt.bat
+++ prebuilt.bat
@@ -6,14 +6,14 @@ set DEST_DIR=%CD%\node_modules\tileserver-gl-light\node_modules\tileserver-gl-st
 if not exist "%DEST_DIR%" (
     echo Destination directory does not exist. Creating...
     mkdir "%DEST_DIR%"
+    :: Copy files if the source directory exists
+    if exist "%SOURCE_DIR%" (
+        echo Copying files...
+        xcopy /E /I "%SOURCE_DIR%" "%DEST_DIR%"
+    ) else (
+        echo Source directory does not exist. Skipping copy.
+    )
 )
 
-:: Copy files if the source directory exists
-if exist "%SOURCE_DIR%" (
-    echo Copying files...
-    xcopy /E /I "%SOURCE_DIR%" "%DEST_DIR%"
-) else (
-    echo Source directory does not exist. Skipping copy.
-)
 
 exit /b 0
--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 const express = require("express");
 const { exec } = require("child_process");
 const { createProxyMiddleware } = require("http-proxy-middleware");
+const cors = require('cors')
 const path = require("path");
+const sqlite3 = require("sqlite3").verbose();
 
 const app = express();
+
+app.use(cors());
+
 const PORT = process.env.APP_PORT || 3000; // Backend port
 const TILESERVER_PORT = process.env.TILESERVER_PORT || 8080; // TileServer GL Light port
-
 // Path to MBTiles file
 const MBTILES_PATH = path.join(__dirname, "../data", "osm-2020-02-10-v3.11_japan_nagoya.mbtiles");
+const DB_PATH = path.join(__dirname, "../data", "nodes.sqlite3");
 
+
+const db = new sqlite3.Database(DB_PATH, (err) => {
+    if (err) {
+        console.error("Error opening DB_PATH database:", err.message);
+    } else {
+        console.log("Connected to DB_PATH database.");
+    }
+});
 // Start TileServer GL Light
 const tileserver = exec(
     `tileserver-gl-light --port ${TILESERVER_PORT} --mbtiles ${MBTILES_PATH}`,
@@ -19,6 +32,37 @@ const tileserver = exec(
         console.log(`TileServerGL Light running at http://localhost:${TILESERVER_PORT}`);
     }
 );
+
+app.get("/search", (req, res) => {
+    const { location } = req.query
+    if (!location) {
+        return res.status(400).json({ error: "Query is required" });
+    }
+
+    const sql = `
+    SELECT id, name, latitude, longitude, kname, laname
+    FROM nodes
+    WHERE name LIKE ? OR kname LIKE ? OR laname LIKE ?
+    LIMIT 20;
+    `;
+    console.log('[Tanga]', location)
+    const params = [`%${location}%`, `%${location}%`, `%${location}%`];
+
+
+    db.all(sql, params, (err, rows) => {
+        console.log('[asdfasdf]', rows, err)
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json(rows.map(r=>({
+            ...r, 
+            lat: r['latitude'],
+            lon: r['longitude'],
+            display_name: r['name'] || r['kname'] || r['laname']
+        })));
+    });
+});
 
 app.use(
     "/tiles",
